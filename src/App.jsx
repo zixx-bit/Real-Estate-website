498c45aa3aea4a3910551ce6dcae2b4a1a050408
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-09-02 21:58:27 +0300

    client

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,11 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
+import Website from "./pages/Website";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <div className="white-gradient" />
-        <Header />
-        <Hero />
-      </div>
-      <Companies />
-      <Residencies/>
-      <Value/>
-      <Contact/>
-      <GetStarted/>
-      <Footer/>
-    </div>
+    <Website/>
   );
 }
 
---
+++ src/pages/Website.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Website = () => {
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies/>
+      <Value/>
+      <Contact/>
+      <GetStarted/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Website
